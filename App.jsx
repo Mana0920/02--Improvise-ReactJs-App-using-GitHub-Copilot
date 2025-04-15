dcd6a9c9ee7842fda0a6ad114055928b0f6c6738
Mana0920 <manali.v.nikam@accenture.com>
2025-04-15 20:55:40 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,10 @@
+const siteProps = {
+  name: "Manali",
+  title: "Oracle Application DBA",
+  email: "manalikhude535@gmail.com",
+  gitHub: "ManaGitHub",
+  instagram: "ManaInstaHandle",
+  linkedIn: "ManaLinkedIn",
+  medium: "ManaMedium",
+  twitter: "ManaTwitter"
+};
